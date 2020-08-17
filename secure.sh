161681b779d6e633d278cd7da8563ac51d27a6c5
Chris Titus <dfm.titus@gmail.com>
2020-08-17 15:29:43 -0500

    Update secure.sh

--- secure.sh
+++ secure.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+#-----------------------
+#--Required Packages-
+#-ufw
+#-fail2ban
 
+# --- Setup UFW rules
+sudo ufw limit 22/tcp  
+sudo ufw allow 80/tcp  
+sudo ufw allow 443/tcp  
+sudo ufw default deny incoming  
+sudo ufw default allow outgoing
+sudo ufw enable
+
+# --- Harden /etc/sysctl.conf
 sysctl kernel.modules_disabled=1
 sysctl -a
 sysctl -A
 sysctl mib
 sysctl net.ipv4.conf.all.rp_filter
 sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+
+# --- PREVENT IP SPOOFS
+cat <<EOF > /etc/host.conf
+order bind,hosts
+multi on
+nospoof on
+EOF
+
+# --- Enable fail2ban
+systemctl enable fail2ban
+systemctl start fail2ban
+
+echo "listening ports"
+netstat -tunlp
