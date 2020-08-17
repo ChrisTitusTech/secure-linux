8dedbe31d38bb557475c2aa88fef79cb8ec42c9d
ChrisTitusTech <contact@christitus.com>
2020-08-17 15:48:33 -0500

    sudo fix

--- secure.sh
+++ secure.sh
@@ -13,12 +13,12 @@ sudo ufw default allow outgoing
 sudo ufw enable
 
 # --- Harden /etc/sysctl.conf
-sysctl kernel.modules_disabled=1
-sysctl -a
-sysctl -A
-sysctl mib
-sysctl net.ipv4.conf.all.rp_filter
-sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+sudo sysctl kernel.modules_disabled=1
+sudo sysctl -a
+sudo sysctl -A
+sudo sysctl mib
+sudo sysctl net.ipv4.conf.all.rp_filter
+sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
 
 # --- PREVENT IP SPOOFS
 cat <<EOF > /etc/host.conf
@@ -27,8 +27,8 @@ multi on
 EOF
 
 # --- Enable fail2ban
-systemctl enable fail2ban
-systemctl start fail2ban
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
 
 echo "listening ports"
-netstat -tunlp
+sudo netstat -tunlp
